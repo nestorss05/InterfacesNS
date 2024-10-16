a9e1784a11754b2c94d95d10ccecad1e7c88b6f9
nestorss05 <nestor.sanchez@iesnervion.es>
2024-10-16 13:48:41 +0200

    Primer commit

---
+++ EjerciciosTema8/EjerciciosTema8/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace EjerciciosTema8
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
