b9e3c5b819004a821a990134fdc41d859b8a56ed
nestorss05 <nestor.sanchez@iesnervion.es>
2024-10-16 17:26:44 +0200

    Otro ejercicio del tema 3

---
+++ Ejercicio1Tema3/BibliotecaClases/EjercicioForms/Form1.Designer.vb
@@ -0,0 +1,121 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        nombre = New Label()
+        Button1 = New Button()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        Label1 = New Label()
+        TextBox3 = New TextBox()
+        Label2 = New Label()
+        SuspendLayout()
+        ' 
+        ' nombre
+        ' 
+        nombre.AutoSize = True
+        nombre.Location = New Point(302, 133)
+        nombre.Name = "nombre"
+        nombre.Size = New Size(71, 20)
+        nombre.TabIndex = 0
+        nombre.Text = "Nombre: "
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(351, 259)
+        Button1.Margin = New Padding(3, 4, 3, 4)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(86, 31)
+        Button1.TabIndex = 1
+        Button1.Text = "Saludo"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(374, 129)
+        TextBox1.Margin = New Padding(3, 4, 3, 4)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(114, 27)
+        TextBox1.TabIndex = 2
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(374, 164)
+        TextBox2.Margin = New Padding(3, 4, 3, 4)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(114, 27)
+        TextBox2.TabIndex = 4
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(294, 167)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(79, 20)
+        Label1.TabIndex = 3
+        Label1.Text = "Apellidos: "
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(374, 199)
+        TextBox3.Margin = New Padding(3, 4, 3, 4)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(114, 27)
+        TextBox3.TabIndex = 6
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(220, 202)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(153, 20)
+        Label2.TabIndex = 5
+        Label2.Text = "Fecha de nacimiento: "
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 451)
+        Controls.Add(TextBox3)
+        Controls.Add(Label2)
+        Controls.Add(TextBox2)
+        Controls.Add(Label1)
+        Controls.Add(TextBox1)
+        Controls.Add(Button1)
+        Controls.Add(nombre)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents nombre As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label2 As Label
+
+End Class
---
+++ Ejercicio1Tema3/BibliotecaClases/EjercicioForms/Form1.vb
@@ -0,0 +1,27 @@
+﻿Imports BibliotecaClases
+Imports System.Runtime.ConstrainedExecution
+
+''' <summary>
+''' Clase Form1: pantalla principal
+''' </summary>
+Public Class Form1
+
+    ''' <summary>
+    ''' Evento asociado al boton encontrado en la pantalla de relleno de datos
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim per As New ClsPersona()
+        If Not TextBox1.Text = "" Then
+            per.Nombre = TextBox1.Text
+        End If
+        If Not TextBox2.Text = "" Then
+            per.Apellido = TextBox2.Text
+        End If
+        If Not TextBox3.Text = "" Then
+            per.FechaNac = DateTime.Parse(TextBox3.Text)
+        End If
+        MsgBox("Hola " + per.Nombre + " " + per.Apellido + ". Tienes " + CStr(per.Edad) + " anios.", vbEmpty, "")
+    End Sub
+End Class
