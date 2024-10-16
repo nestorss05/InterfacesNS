9f897746e966a06b650ccd5c03fa01a39458e2d3
nestorss05 <nestor.sanchez@iesnervion.es>
2024-10-16 13:52:27 +0200

    Rev

--- HolaMundo/HolaMundo/obj/Debug/net8.0-windows/HolaMundo.AssemblyInfo.cs
+++ HolaMundo/HolaMundo/obj/Debug/net8.0-windows/HolaMundo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HolaMundo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9e1784a11754b2c94d95d10ccecad1e7c88b6f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("HolaMundo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HolaMundo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
