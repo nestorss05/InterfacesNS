6ddaf1d075a1e75399b11f958bec258a33a10940
nestorss05 <nestor.sanchez@iesnervion.es>
2024-10-18 10:12:28 +0200

    .

--- EjerciciosTema8/Ejercicio5/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/Ejercicio5.AssemblyInfo.cs
+++ EjerciciosTema8/Ejercicio5/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/Ejercicio5.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ejercicio5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+53a5bee8d579893101093246d8ae86c14faa8a36")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+a5db9a011b86a6f99dafe2a86e220001d073f323")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ejercicio5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ejercicio5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
