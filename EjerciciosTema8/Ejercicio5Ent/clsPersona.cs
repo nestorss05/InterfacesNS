fb0263cb958eda9f9ddfb4b5a3056e56e9f576e5
nestorss05 <nestor.sanchez@iesnervion.es>
2024-10-22 19:50:31 +0200

    Ejercicio 5 terminado

---
+++ EjerciciosTema8/Ejercicio5Ent/clsPersona.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio5Ent
+{
+    public class clsPersona
+    {
+        public int id { get; set; }
+        public required string nombre { get; set; }
+        public required string apellidos { get; set; }
+        public required string fechaNac { get; set; }
+    }
+}
