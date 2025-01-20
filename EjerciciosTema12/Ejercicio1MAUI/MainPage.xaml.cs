814a014d63f8bb2c96b9b5c748e47a30dd53368f
nestorss05 <nestor.sanchez@iesnervion.es>
2025-01-20 14:20:20 +0100

    Ejercicio SignalR casi completo
    Falta el de MAUI

---
+++ EjerciciosTema12/Ejercicio1MAUI/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+namespace Ejercicio1MAUI
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly HubConnection _connection;
+        public MainPage()
+        {
+            InitializeComponent();
+            _connection = new HubConnectionBuilder()
+                .WithUrl("http://localhost:7237")
+                .Build();
+            _connection.On<string>("MessageRecieved");
+            // https://www.youtube.com/watch?v=pDr0Hx67guk
+            // 11:46
+        }
+
+        private void Send(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+
+}
---
+++ EjerciciosTema12/Models/clsMensajeUsuario.cs
@@ -0,0 +1,8 @@
+﻿namespace Models
+{
+    public class clsMensajeUsuario
+    {
+        public String nombreUsuario { get; set; }
+        public String mensajeUsuario { get; set; }
+    }
+}
